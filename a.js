4848bc96b412ced9dbabda1fa703eeff47dca598
Exon <exon038@gmail.com>
2026-05-30 19:38:03 +0200

    Update a.js

--- a.js
+++ a.js
@@ -68,7 +68,7 @@ async function iframe() {
 };
 
 (async function() {
-	if (ur.searchParams.get("iframetrap") === "true") {
+	if (ur.searchParams.get("iframetrap") === true) {
 		if (!document.body.querySelector('iframe[class=EDsploit]')) {
 			return null
 		} else {
